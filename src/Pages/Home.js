d5e2a70e9e09543ffeaa6970c444d7f30e07c7d4
Conor Whyte <conor.whyte@ucdconnect.ie>
2018-11-06 21:22:29 +0000

    Navbar mostly working

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -30,10 +30,16 @@ class Home extends Component {
       showGutter: false,
       paddingLeft: null,
       showOnlyMarkdown: false,
+      showSplitPanel: false,
     };
 
+    this.showGutter = this.showGutter.bind(this);
+    this.searchForString = this.searchForString.bind(this);
     this.changeEditorTheme = this.changeEditorTheme.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.changeEditorMode = this.changeEditorMode.bind(this);
+    this.previewMarkdown = this.previewMarkdown.bind(this);
+    this.splitMarkdownEditor = this.splitMarkdownEditor.bind(this);
   }
   
   handleChange(newValue) {
@@ -43,17 +49,45 @@ class Home extends Component {
   }
 
   changeEditorTheme(theme) {
-    this.setState({
-      theme,
+    this.setState({ theme });
+    this.changeBackgroundColour(theme);
+  }
+
+  changeBackgroundColour(theme) {
+    switch (theme) {
+      case 'textmate': 
+        document.body.style = 'background: white;';
+        break;
+      case 'kuroir': 
+        document.body.style = 'background: #E8E9E8;';
+        break;
+      case 'monokai': 
+        document.body.style = 'background: #272722;';
+        break;
+    }
+  }
+
+  showGutter(showGutter) {
+    this.setState({ 
+      showGutter,
+      paddingLeft: showGutter ? '0px' : null, 
     });
   }
 
-  changeTheme() {
-    this.setState({
-      theme: "kuroir",
-      mode: "json",
-      showGutter: true,
-    });
+  changeEditorMode(mode) {
+    this.setState({ mode });
+  }
+
+  splitMarkdownEditor() {
+    this.setState(prevState => ({
+      showSplitPanel: !prevState.showSplitPanel
+    })); 
+  }
+
+  previewMarkdown() {
+    this.setState(prevState => ({
+      showOnlyMarkdown: !prevState.showOnlyMarkdown
+    })); 
   }
 
   jsonToYaml() {
@@ -81,6 +115,18 @@ class Home extends Component {
     const { value } = this.state;
     document.cookie = `${COOKIE_VALUE}${value}`;
   }
+
+  searchForString(searchString) {
+    const reactAceComponent = this.refs.reactAceComponent;
+    const editor = reactAceComponent.editor;
+    editor.findAll(searchString, {
+      backwards: false,
+      wrap: true,
+      caseSensitive: false,
+      wholeWord: false,
+      regExp: true,
+    });
+  }
   
   getCookie() {
     const { cookie } = document;
@@ -101,24 +147,42 @@ class Home extends Component {
   optionsBar() {
     return (
       <div className ="optionsBar">
-        <button onClick={() => { this.changeTheme(); }}> JSON </button>
+        {/* <button onClick={() => { this.changeTheme(); }}> JSON </button>
         <button onClick={() => { this.jsonToYaml(); }}> YAML </button>
-        <button onClick={() => { this.format(); }}> FORMAT </button>
+        <button onClick={() => { this.format(); }}> FORMAT </button> */}
         <button onClick={() => { this.saveToCookie(); }}> SAVE </button>
-        <button onClick={() => { this.getCookie(); }}> GET </button>
+        {/* <button onClick={() => { this.getCookie(); }}> GET </button> */}
       </div>
     ); 
   }
 
   render() {
-    const { theme, mode, value, showGutter, fontSize, tabSize, paddingLeft, showOnlyMarkdown } = this.state;
-    const editorWidth = showOnlyMarkdown ? '50%' : '100%';
+    const { 
+      theme, 
+      mode, 
+      value, 
+      showGutter, 
+      fontSize, 
+      tabSize, 
+      paddingLeft, 
+      showOnlyMarkdown, 
+      showSplitPanel 
+    } = this.state;
+    const editorWidth = showSplitPanel ? '50%' : '100%';
+    const markdownWidth = showOnlyMarkdown ? '100%' : '45%';
+    const markdownPadding = showOnlyMarkdown ? '10px' : null;
+
     return (
       <div className="App">
-        <Nav changeEditorTheme={this.changeEditorTheme} />
-        {/* { this.optionsBar() } */}
+        <Nav  
+          changeEditorTheme={this.changeEditorTheme} 
+          searchForString={this.searchForString}
+          changeEditorMode={this.changeEditorMode}
+          showGutter={this.showGutter}
+          previewMarkdown={this.previewMarkdown}
+          splitMarkdownEditor={this.splitMarkdownEditor} />
         <div className="Editor" style={{paddingLeft, width: editorWidth}}>  
-          <AceEditor
+          { !showOnlyMarkdown && <AceEditor
             mode={mode}
             theme={theme}
             value={value}
@@ -130,11 +194,14 @@ class Home extends Component {
             tabSize={tabSize}
             fontSize={fontSize}
             onChange={this.handleChange}
-            name="TextInputEditor" 
-          />
+            setOptions={{
+              highlightSelectedWord: true
+            }}
+            name="aceEditor" 
+          ref="reactAceComponent" /> }
         </div>
-        {showOnlyMarkdown && (
-          <div style={{width: '45%', float: 'right'}}>
+        { ( showOnlyMarkdown || showSplitPanel ) && (
+          <div style={{width: markdownWidth, float: 'right', paddingLeft: markdownPadding}}>
             <ReactMarkdown source={value}/>
           </div>
         )}
--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -24,85 +24,116 @@ export default class Nav extends Component {
       };
     }
 
+    checkboxClick(e) {
+      this.props.showGutter(e.target.checked);
+    }
+
+    onSearch = (e) => {
+      const searchString = e.target.value;
+      this.props.searchForString(searchString);
+    }
+
+    rightSideNavbar() {
+      return (
+        <NavbarNav right>
+          <NavItem style={{color:'white', paddingRight: '10px'}}>
+            Show Gutter
+            <input  type="checkbox" 
+                    name="gutter" 
+                    onChange={this.checkboxClick.bind(this)}
+                    style={{marginLeft: '10px', marginRight: '10px'}}/>
+          </NavItem>
+          <NavItem style={{height: '20px'}}>
+            <form className="form-inline md-form mt-0" style={{height: '18px'}}>
+              <input  className="form-control mr-sm-2 mb-0 text-white" 
+                      type="text" 
+                      placeholder="Search"
+                      style={{height: '20px', paddingBottom: '0.05rem'}} 
+                      aria-label="Search"
+                      onChange={this.onSearch} />
+            </form>
+          </NavItem>
+        </NavbarNav>
+      );
+    }
+
+    yamlJsonDropdown(header) {
+      return (
+        <NavItem>
+          <Dropdown>
+            <DropdownToggle nav style={{fontWeight: '500'}}>{header}</DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem href="#">Format</DropdownItem>
+              <DropdownItem href="#">Validate</DropdownItem>
+              <DropdownItem href="#">Convert</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavItem>
+      );
+    }
+
+    previewMarkdown() {
+      this.props.previewMarkdown();
+    }
+
+    splitMarkdownEditor() {
+      this.props.splitMarkdownEditor();
+    }
+
+    markdownDropdown() {
+      return (
+        <NavItem>
+          <Dropdown>
+            <DropdownToggle nav style={{fontWeight: '500'}}>Markdown</DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem href="#" onClick={() => this.previewMarkdown()}>Preview</DropdownItem>
+              <DropdownItem href="#" onClick={() => this.splitMarkdownEditor()}>Double pane</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavItem>
+      );
+    }
+
+    mapDropdown(items, header, onClickFunc) {
+      return (
+        <NavItem>
+          <Dropdown>
+            <DropdownToggle nav style={{fontWeight: '500'}}>{header}</DropdownToggle>
+            <DropdownMenu>
+            { items.map((value) => 
+                <DropdownItem href="#" onClick={() => onClickFunc(value)} key={value}>
+                  {value}
+                </DropdownItem> 
+              )}
+            </DropdownMenu>
+          </Dropdown>
+        </NavItem>
+      );
+    }
+
     render() {
       const { styles, modes } = this.state;
+      const { changeEditorTheme, changeEditorMode } = this.props;
+      
       return (
         <Navbar color="elegant-color" dark expand="md" scrolling>
           <NavbarBrand href="#" style={{marginLeft: '-7px'}}>
             <img src={NotepadImage} height="30"/>
           </NavbarBrand>
           <NavbarNav left>
-            <NavItem>
-            <Dropdown>
-              <DropdownToggle nav style={{fontWeight: '500'}}>Json</DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem href="#">Format</DropdownItem>
-                <DropdownItem href="#">Validate</DropdownItem>
-                <DropdownItem href="#">Convert to Yaml</DropdownItem>
-              </DropdownMenu>
-              </Dropdown>
-            </NavItem>
-            <NavItem>
-            <Dropdown>
-              <DropdownToggle nav style={{fontWeight: '500'}}>Yaml</DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem href="#">Format</DropdownItem>
-                <DropdownItem href="#">Validate</DropdownItem>
-                <DropdownItem href="#">Convert to Json</DropdownItem>
-              </DropdownMenu>
-              </Dropdown>
-            </NavItem>
-            <NavItem>
-              <Dropdown>
-                <DropdownToggle nav style={{fontWeight: '500'}}>Markdown</DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem href="#">Preview</DropdownItem>
-                  <DropdownItem href="#">Double pane</DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-            </NavItem>
-            <NavItem>
-              <Dropdown>
-                <DropdownToggle nav style={{fontWeight: '500'}}>Style</DropdownToggle>
-                <DropdownMenu>
-                  { styles.map((value) => 
-                    <DropdownItem href="#" onClick={() => this.props.changeEditorTheme(value)} key={value}>
-                      {value}
-                    </DropdownItem> 
-                  )}
-                </DropdownMenu>
-              </Dropdown>
-            </NavItem>
-            <NavItem>
-              <Dropdown>
-                <DropdownToggle nav style={{fontWeight: '500'}}>Mode</DropdownToggle>
-                <DropdownMenu>
-                { modes.map((value) => 
-                    <DropdownItem href="#" onClick={() => this.props.changeEditorTheme(value)} key={value}>
-                      {value}
-                    </DropdownItem> 
-                  )}
-                </DropdownMenu>
-              </Dropdown>
-            </NavItem>
+            { this.yamlJsonDropdown('Json') }
+            
+            { this.yamlJsonDropdown('Yaml') }
+            
+            { this.markdownDropdown() }
+
+            { this.mapDropdown(styles, 'Style', changeEditorTheme) }
+
+            { this.mapDropdown(modes, 'Mode', changeEditorMode) }
           </NavbarNav>
-          <NavbarNav right>
-            <NavItem style={{color:'white', paddingRight: '10px'}}>
-              Show Gutter
-              <input  type="checkbox" 
-                      name="gutter" 
-                      style={{marginLeft: '10px', marginRight: '10px'}}/>
-            </NavItem>
-            <NavItem style={{height: '20px'}}>
-              <form className="form-inline md-form mt-0" style={{height: '18px'}}>
-                <input  className="form-control mr-sm-2 mb-0 text-white" 
-                        type="text" 
-                        placeholder="Search"
-                        style={{height: '20px', paddingBottom: '0.05rem'}} 
-                        aria-label="Search" />
-              </form>
-            </NavItem>
-          </NavbarNav>
+
+          { this.rightSideNavbar() }
+
         </Navbar>
       );
     }
